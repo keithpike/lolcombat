575b905f0a5c56d28147af8c9594df072a455ed0
Keith Pike <whogotbooboo@yahoo.com>
2015-03-14 17:54:40 -0700

    commit before refactoring breaks things, no new branch for me(exclamation point)

--- app/controllers/api/v1/splashes_controller.rb
+++ app/controllers/api/v1/splashes_controller.rb
@@ -1,5 +1,5 @@
 module Api::V1
-	class SplashesController < Api::V1::ApplicationController
+	class SplashesController < Api::V1::BaseController
 		protect_from_forgery with: :null_session
 	end
 end
